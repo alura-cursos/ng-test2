05609776b815142dc58f0634bec6803d5f257a10
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-12-02 15:21:42 +0000

    video4.5

--- src/app/shared/components/photo-board/photo-board.component.ts
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -22,7 +22,7 @@ export class PhotoBoardComponent implements OnChanges {
     const newRows = [];
     const step = 4;
     for (let index = 0; index < photos.length; index += step) {
-      newRows.push(photos.splice(index, index + step));
+      newRows.push(photos.slice(index, index + step));
     }
     return newRows;
   }
