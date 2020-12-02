5f0a1d9b6d6555e1659bf87e6735295f2ad95361
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-12-02 12:53:26 +0000

    video3.8

--- src/app/shared/directives/action/action.directive.spec.ts
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ActionDirective } from './action.directive';
 import { ActionDirectiveModule } from './action.module';
@@ -20,7 +21,7 @@ describe(ActionDirective.name, () => {
   });
 
   it(`(D) (@Output appAction) should emit event with payload when ENTER key is pressed`, () => {
-    const divEl: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const divEl = fixture.debugElement.query(By.directive(ActionDirective)).nativeElement;
     const event = new KeyboardEvent('keyup', { key: 'Enter' });
     divEl.dispatchEvent(event);
     expect(component.hasEvent()).toBeTrue();
