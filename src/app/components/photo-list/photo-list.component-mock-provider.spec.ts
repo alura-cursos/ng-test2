5cfd25106489f1548540e09f3ed5f93491dfc6ed
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-12-03 17:08:12 +0000

    video5.12

--- src/app/components/photo-list/photo-list.component-mock-provider.spec.ts
+++ src/app/components/photo-list/photo-list.component-mock-provider.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
 import { Photo } from 'src/app/shared/components/photo-board/interfaces/photo';
+import { PhotoBoardMockService } from 'src/app/shared/components/photo-board/services/photo-board-mock.service';
 import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 import { buildPhotoList } from 'src/app/shared/components/photo-board/test/build-photo-list';
 import { PhotoListComponent } from './photo-list.component';
@@ -20,12 +21,8 @@ describe(PhotoListComponent.name + ' Mock Provider', () => {
       providers: [
         {
           provide: PhotoBoardService,
-          useValue: {
-            getPhotos(): Observable<Photo[]> {
-              return of(buildPhotoList());
-            }
-        }
-      }]
+          useClass: PhotoBoardMockService
+        }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PhotoListComponent);
